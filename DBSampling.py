2dcdd05d5a90c5a3ebf793c2d1171027e9a97761
Ga Young Lee <leegay@oregonstate.edu>
2020-11-13 11:03:11 -0800

    part III

--- DBSampling.py
+++ DBSampling.py
@@ -7,6 +7,9 @@ import statistics
 import numpy as np
 
 # Part I. Load the csv files into data frames
+file_name0 = 'omdb_poster20_repair.csv'
+df_0 = pd.read_csv(file_name0)
+
 file_name1 = 'omdb_rating5_repair.csv'
 df_1 = pd.read_csv(file_name1)
 
@@ -31,13 +34,10 @@ df_7 = pd.read_csv(file_name7)
 file_name8 = 'omdb_poster10_repair.csv'
 df_8 = pd.read_csv(file_name8)
 
-file_name9 = 'omdb_poster20_repair.csv'
-df_9 = pd.read_csv(file_name9)
-
 
 # Part II. Randomly Sample DB
 # Assumption: Normal distribution (Gaussian) of the DB indexes
-DB_Index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+DB_Index = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 mu = statistics.mean(DB_Index) # Mean
 sigma = statistics.pstdev(DB_Index) # Standard Deviation
 
@@ -45,3 +45,9 @@ k = int(input ("Enter the number of DB you'd like to sample: "))
 sample = np.random.normal(mu, sigma, k).round(0)
 
 print("The indexes of sampled DB : ", sample)
+
+# Part III. Retrieve the sampled databases and save them in a separate folder
+compression_opts = dict(method='zip',
+                        archive_name='out.csv')
+df_"int(sample[0])".to_csv('out.zip', index=False,
+          compression=compression_opts)
